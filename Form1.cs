d1aafec6a4af9a942397f0b0fbb433e82c123765
HowdareyouBass <tf2kub1k@gmail.com>
2020-04-01 21:49:37 +0300

    reset picturebox to graphics

--- Form1.cs
+++ Form1.cs
@@ -12,35 +12,95 @@ namespace Project
 {
     public partial class Form1 : Form
     {
+        Image playerImage;
+        Graphics graph;
         private int frame;
         private int animation;
+        Stickman stick;
         public Form1()
         {
             InitializeComponent();
-            this.KeyDown += new KeyEventHandler(keyboard);
+            this.KeyDown += new KeyEventHandler(DownKey);
+            this.KeyUp += new KeyEventHandler(UpKey);
+            playerImage = new Bitmap("D:\\Visual Proj\\Project\\resources\\img\\Stay_stick.png");
+            stick = new Stickman(new Size(45, 78), 0, 150, playerImage, 3);
+            graph = this.CreateGraphics();
+            timer1.Interval = 40;
+            timer2.Interval = 10;
+            timer1.Tick += new EventHandler(Move);
+            timer2.Tick += new EventHandler(Animate);
+            timer1.Start();
+            timer2.Start();
         }
 
-        private void keyboard(object sender, KeyEventArgs e)
+        private void Animate(object sender, EventArgs e)
         {
-           switch (e.KeyCode.ToString())
+            //graph.Clear(Color.White);
+            graph.DrawImage(stick.StickmanImg, new Point(stick.x, stick.y));
+            this.Invalidate();
+        }
+
+        private void UpKey(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString() == "D")
             {
-                case "W":
-                    animation = 3;
+                animation = 0;
+            }
+            else if (e.KeyCode.ToString() == "A")
+            {
+                animation = 0;
+            }
+            if(e.KeyCode.ToString() == "W")
+            {
+                animation = 0;
+            }
+            this.Invalidate();
+        }
+
+        private void Move(object sender, EventArgs e)
+        {
+            switch (animation)
+            {
+                case 0:
                     break;
-                case "D":
-                    animation = 1;
-                    pictureBox1.Location = new Point(pictureBox1.Location.X + 2,pictureBox1.Location.Y);
+                case 1:
+                    stick.Right();
                     break;
-                case "A":
-                    animation = 2;
-                    pictureBox1.Location = new Point(pictureBox1.Location.X - 2, pictureBox1.Location.Y);
+                case 2:
+                    stick.Left();
                     break;
+                case 3:
+                    break;
+            }
+            this.Invalidate();
+        }
+
+
+        private void DownKey(object sender, KeyEventArgs e)
+        {
+           if(e.KeyCode.ToString() == "D")
+            {
+                animation = 1;
+            }
+           else if(e.KeyCode.ToString() == "A")
+            {
+                animation = 2;
+            }
+           if(e.KeyCode.ToString() == "W")
+            {
+                animation = 3;
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            Graphics gro = e.Graphics;
+            gro.DrawImage(stick.StickmanImg, new Point(stick.x, stick.y));
         }
     }
 }
---
+++ Stickman.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Stickman
+    {
+        public Image StickmanImg;
+        public int x, y;
+        public Size Siz;
+        public int frame;
+        public int animation;
+        public int speed;
+        public Stickman(Size Siz,int x,int y,Image StickmanImg,int speed)
+        {
+            this.x = x;
+            this.y = y;
+            this.Siz = Siz;
+            this.StickmanImg = StickmanImg;
+            this.speed = speed;
+        }
+        public void Left()
+        {
+            x -= speed;
+        }
+        public void Right()
+        {
+            x += speed;
+        }
+        public void Jump()
+        {
+
+        }
+    }
+}
